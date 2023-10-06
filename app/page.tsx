8ee6646bb7b6b6bd8ec1a5fb1148285df99e72e1
iamzubair6 <zubair.rahman89@gmail.com>
2023-10-06 21:01:21 +0600

    Initial project setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Nav from "@/components/Nav/Nav";
+
+export default function Home() {
+  return (
+    <main className="flex justify-center flex-col p-24">
+      <Nav />
+      <section className="mt-20">
+        <h1>Testing out shadcn ui with typescript</h1>
+      </section>
+    </main>
+  );
+}
